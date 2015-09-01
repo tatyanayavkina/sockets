89427dea462e8ba01ed7fee3ed209a3b8c793e09
tatyanayavkina <t.yavkina@mail.ru>
2015-09-01 16:24:44 +0300

    add Message class

---
+++ src/Message.java
@@ -0,0 +1,45 @@
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Created by Татьяна on 01.09.2015.
+ */
+public class Message implements Serializable{
+    private String author;
+    private String IP;
+    private LocalDateTime creationTime;
+    private String content;
+
+    public Message(String author, String IP, String content){
+        this.author = author;
+        this.IP = IP;
+        this.content = content;
+        this.creationTime = LocalDateTime.now();
+    }
+
+    public String write(){
+        StringBuilder builder = new StringBuilder();
+        String output;
+
+        // add author
+        builder.append("author: ");
+        builder.append(author);
+        builder.append("\n");
+        //add IP
+        builder.append("IP: ");
+        builder.append(IP);
+        builder.append("\n");
+        //add time
+        builder.append("creation time: ");
+        builder.append(creationTime);
+        builder.append("\n");
+        //add content
+        builder.append(content);
+        builder.append("\n");
+
+        output = builder.toString();
+
+        return output;
+    }
+
+}
